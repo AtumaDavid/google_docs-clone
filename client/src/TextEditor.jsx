141095675927df76f1a4b038514048f8bae576c6
AtumaDavid <atumadavid35@gmail.com>
2024-02-29 21:27:53 +0100

    first commit

---
+++ client/src/TextEditor.jsx
@@ -0,0 +1,16 @@
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+import { useCallback } from "react";
+
+export default function TextEditor() {
+  //   const wrapperRef = useRef();
+
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    new Quill(editor, { theme: "snow" });
+  }, []);
+  return <div className="container" ref={wrapperRef}></div>;
+}
